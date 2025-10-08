0dc079b38d6a1294a85cfedb743e36ee034657e2
Eesha <eesha.jain08@gmail.com>
2025-10-07 23:21:24 -0400

    Update WireHopperInfo.cs

--- WireHopper/WireHopperInfo.cs
+++ WireHopper/WireHopperInfo.cs
@@ -13,17 +13,18 @@ namespace WireHopper
         public override Bitmap Icon => null;
 
         //Return a short string describing the purpose of this GHA library.
-        public override string Description => "";
+        public override string Description => "A Grasshopper plugin for managing, visualizing, and cleaning wires.";
 
         public override Guid Id => new Guid("234d6516-f807-4001-b11a-36b5fc68c658");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "Eesha Jain";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "toeesha@gmail.com";
 
         //Return a string representing the version.  This returns the same version as the assembly.
-        public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+        public override string AssemblyVersion => 1.0.0;
     }
+
 }
